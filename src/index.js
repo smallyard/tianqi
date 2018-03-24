e1e8d95625bd3ca9eec0ac55499fc448ddcf2d68
smallyard <liuhongqiang@live.cn>
2018-03-24 17:17:29 +0800

    fix api

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
-var request = require("request");
-var config = require("./config");
 var print = require("./print");
-
-module.exports = function (word) {
-    if (word) {
-        getDataByCityName(word);
-    } else {
-        request.get(config.ip.url, function (error, response, body) {
-            var ipResult = JSON.parse(body);
-            var options = {
-                url: config.ipToCity.url + ipResult.ip,
-                headers: {
-                    "apikey": config.ipToCity.key
-                }
-            };
-            request.get(options, function (error, response, body) {
-                var cityNameResult = JSON.parse(body);
-                getDataByCityName(cityNameResult.retData.city);
-            });
-        });
+module.exports = function(city) {
+    if (!city) {
+        city = "ip";
     }
-};
 
-function getDataByCityName(word) {
-    word = encodeURIComponent(word);
-    var options = {
-        url: config.weather.url + word,
-        headers: {
-            "apikey": config.weather.key
-        }
-    };
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            print.print(JSON.parse(body));
-        }
+    const UID = "U785B76FC9";
+    const KEY = "4r9bergjetiv1tsd";
+    var Api = require('./lib/api.js');
+    var api = new Api(UID, KEY);
+
+    api.getWeatherNow(city).then(function(data) {
+        print.printToday(data, null, 4);
+        api.getSuggestion(city).then(function(data) {
+            print.printSuggestion(data);
+            api.getDaily(city).then(function(data) {
+                print.printDaily(data);
+            }).catch(function(err) {
+               console.log(err.red);
+            });
+        }).catch(function(err) {
+           console.log(err.red);
+        });
+    }).catch(function(err) {
+       console.log(err.red);
     });
-}
+};
---
+++ src/lib/api.js
@@ -0,0 +1,69 @@
+
+
+/**
+ * Module denpendencies
+ */
+var crypto = require('crypto');
+var querystring = require('querystring');
+var request = require('request-promise');
+
+var URL = 'https://api.seniverse.com/v3/';
+
+function Api(uid, secretKey) {
+  this.uid = uid;
+  this.secretKey = secretKey;
+}
+
+Api.prototype.getSignatureParams = function() {
+
+  var params = {}
+  params.ts = Math.floor((new Date()).getTime() / 1000); // 当前时间戳（秒）
+  params.ttl = 300; // 过期时间
+  params.uid = this.uid; // 用户ID
+
+  var str = querystring.encode(params); // 构造请求字符串
+  // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串进行加密
+  params.sig = crypto.createHmac('sha1', this.secretKey)
+    .update(str)
+    .digest('base64'); // 将加密结果用 base64 编码，并做一个 urlencode，得到签名 sig
+
+  return params;
+}
+
+Api.prototype.getWeatherNow = function(location) {
+  var params = this.getSignatureParams();
+  params.location = location;
+
+  // 将构造的 URL 直接在后端 server 内调用
+  return request({
+    url: URL + 'weather/now.json',
+    qs : params,
+    json : true
+  });
+}
+
+Api.prototype.getSuggestion = function(location) {
+  var params = this.getSignatureParams();
+  params.location = location;
+
+  // 将构造的 URL 直接在后端 server 内调用
+  return request({
+    url: URL + 'life/suggestion.json',
+    qs : params,
+    json : true
+  });
+}
+
+Api.prototype.getDaily = function(location) {
+  var params = this.getSignatureParams();
+  params.location = location;
+
+  // 将构造的 URL 直接在后端 server 内调用
+  return request({
+    url: URL + 'weather/daily.json',
+    qs : params,
+    json : true
+  });
+}
+
+module.exports = Api;
